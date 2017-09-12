4f319b4da9611df12d92513b87d43916e169a893
Hewlette Dibble <hcodydibble@gmail.com>
2017-09-12 14:32:07 -0700

    i am going to stop working on the master branch now...removed some unneeded lines of code

--- app.js
+++ app.js
@@ -25,11 +25,7 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   $('#projects').click(function(){
-    $('#stickHere').empty()
-    $('#stickHere').append('<a href="' + projectArray[0].url + '">' + projectArray[0].name + '</a>')
-    $('#stickHere').append('<p>A project where I had to create a random image generator to cycle through a set number of chosen images and allow the user to click on their favorite to vote for it. At the end it will display the number of votes received in a graph and also save the data to local storage so that it will persist through page refreshes</p>')
-    $('#stickHere').append('<a href="' + projectArray[1].url + '">' + projectArray[1].name + '</a>')
-    $('#stickHere').append('<p>A project where myself and two others created a site that would allow a user to either create or find a game of tennis at their court of choice in the Seattle area</p>')
+    $('#stickHere').empty().append('<a href="' + projectArray[0].url + '">' + projectArray[0].name + '</a>','<p>A project where I had to create a random image generator to cycle through a set number of chosen images and allow the user to click on their favorite to vote for it. At the end it will display the number of votes received in a graph and also save the data to local storage so that it will persist through page refreshes</p>','<a href="' + projectArray[1].url + '">' + projectArray[1].name + '</a>','<p>A project where myself and two others created a site that would allow a user to either create or find a game of tennis at their court of choice in the Seattle area</p>')
     $('p').css({'padding-top':'2vh','margin-bottom':'1vh'})
     $('a').css('font-size','4vh')
   })
