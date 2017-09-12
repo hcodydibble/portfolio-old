d9e25c5033b19ffde9013e6c8362945b5e841912
Hewlette Dibble <hcodydibble@gmail.com>
2017-09-12 10:57:01 -0700

    created event handler to change dom when links in nav are clicked, fixed up the css some!

--- app.js
+++ app.js
@@ -6,9 +6,32 @@ function Projects(name, url){
   this.url = url;
   projectArray.push(this);
 }
+new Projects('Bus Mall','https://hcodydibble.github.io/bus-mall/')
+new Projects('Tennis Anyone?','https://gabrielx52.github.io/tennis_anyone/')
 
 $(document).ready(function(){
   $('.fa-bars').click(function(){
-    $('.navi').css('display','block')
+    $('.navi').css({'display':'block','padding':'2vh'})
+  })
+})
+
+$(document).ready(function(){
+  $('#education').click(function(){
+    $('.replace').replaceWith('<h2>Education</h2>','<p>This will be my educational history!</p>')
+    $('.delete').remove()
+    $('h2').css({'font-size':'5vw','color':'currentColor','-webkit-text-stroke':'1px #cc73cf','text-align':'center'})
+  })
+})
+
+$(document).ready(function(){
+  $('#projects').click(function(){
+    $('.replace').remove()
+    $('.delete').remove()
+    $('#stickHere').append('<a href="https://hcodydibble.github.io/bus-mall/">' + projectArray[0].name + '</a>')
+    $('#stickHere').append('<p>A project where I had to create a random image generator to cycle through a set number of chosen images and allow the user to click on their favorite to vote for it. At the end it will display the number of votes received in a graph and also save the data to local storage so that it will persist through page refreshes</p>')
+    $('#stickHere').append('<a href="https://gabrielx52.github.io/tennis_anyone/">' + projectArray[1].name + '</a>')
+    $('#stickHere').append('<p>A project where myself and two others created a site that would allow a user to either create or find a game of tennis at their court of choice in the Seattle area</p>')
+    $('p').css({'padding-top':'2vh','margin-bottom':'1vh'})
+    $('a').css('font-size','4vh')
   })
 })
