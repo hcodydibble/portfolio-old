25f4e954993fdd92615cd267aed1c778428f375b
Hewlette Dibble <hcodydibble@gmail.com>
2017-09-13 14:27:50 -0700

    attempting to make it so education and project wont repeat when being switch between

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var projectArray = [], educationArray = [];
 function Projects(projectData){
   Object.assign(this, projectData)
 }
-function Education(edData){
-  Object.assign(this, edData)
-}
 
 $('.fa-bars').click(function(){
   $('.navi').css({'display':'block','padding':'2vh'})
@@ -14,40 +11,45 @@ $('.fa-bars').click(function(){
 
 $('#education').click(function(){
   $('#stickHere').empty()
+  $('#theProjects').css('display','none')
   educationArray.forEach(function(education){
     $('#theEducation').append(education.toHtml())
   })
+  $('#theEducation').css('display','block')
 })
 
 $('#projects').click(function(){
   $('#stickHere').empty()
+  $('#theEducation').css('display','none')
   projectArray.forEach(function(project){
     $('#theProjects').append(project.toHtml())
   })
+  $('#theProjects').css('display','block')
   $('p').css({'padding-top':'1vh','margin-bottom':'1vh'})
   $('a').css('font-size','4vh')
 })
 
 Projects.prototype.toHtml = function(){
-  var $newProject = $('#projectTemplate').clone()
-  $newProject.attr('id','')
-  $newProject.find('.project a').attr('href',this.url).text(this.name)
-  $newProject.find('#description').html(this.description)
-  return $newProject
-}
-Education.prototype.toHtml = function(){
-  var $newEd = $('#edTemplate').clone()
-  $newEd.attr('id','')
-  $newEd.find('h2').text(this.name)
-  $newEd.find('.years').text(this.years)
-  $newEd.find('.study').text(this.study)
-  return $newEd
+  if(this.url){
+    var $newProject = $('#projectTemplate').clone()
+    $newProject.attr('id','')
+    $newProject.find('.project a').attr('href',this.url).text(this.name)
+    $newProject.find('#description').html(this.description)
+    return $newProject
+  }else{
+    var $newEd = $('#edTemplate').clone()
+    $newEd.attr('id','')
+    $newEd.find('h2').text(this.name)
+    $newEd.find('.years').text(this.years)
+    $newEd.find('.study').text(this.study)
+    return $newEd
+  }
 }
 
 myInfo.forEach(function(info){
   if(info.url){
     projectArray.push(new Projects(info))
   }else{
-    educationArray.push(new Education(info))
+    educationArray.push(new Projects(info))
   }
 })
