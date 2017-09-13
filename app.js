8879da206d7589b74afed000baf672a28fcb82a0
Hewlette Dibble <hcodydibble@gmail.com>
2017-09-13 11:39:20 -0700

    added more jquery, hating my education array

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 'use strict';
 var projectArray = [];
 
-function Projects(name, url){
-  this.name = name;
-  this.url = url;
-  projectArray.push(this);
+function Projects(projectData){
+  this.name = projectData.name;
+  this.url = projectData.url;
+  this.description = projectData.description;
 }
-new Projects('Bus Mall','https://hcodydibble.github.io/bus-mall/')
-new Projects('Tennis Anyone?','https://gabrielx52.github.io/tennis_anyone/')
+function Education(edData){
+  this.name = edData.name;
+  this.years = edData.years;
+  this.study = edData.study;
+}
 
-$(document).ready(function(){
-  $('.fa-bars').click(function(){
-    $('.navi').css({'display':'block','padding':'2vh'})
-  })
+$('.fa-bars').click(function(){
+  $('.navi').css({'display':'block','padding':'2vh'})
 })
 
-$(document).ready(function(){
-  $('#education').click(function(){
-    $('.replace').replaceWith('<h2>Education</h2>','<p>This will be my educational history!</p>')
-    $('.delete').remove()
-    $('h2').css({'font-size':'5vw','color':'currentColor','-webkit-text-stroke':'1px #cc73cf','text-align':'center'})
-  })
+$('#education').click(function(){
+  $('.replace').replaceWith('<h2>Education</h2>','<p>This will be my educational history!</p>')
+  $('.delete').remove()
+  $('h2').css({'font-size':'5vw','color':'currentColor','-webkit-text-stroke':'1px #cc73cf','text-align':'center'})
 })
 
-$(document).ready(function(){
-  $('#projects').click(function(){
-    $('#stickHere').empty().append('<a href="' + projectArray[0].url + '">' + projectArray[0].name + '</a>','<p>A project where I had to create a random image generator to cycle through a set number of chosen images and allow the user to click on their favorite to vote for it. At the end it will display the number of votes received in a graph and also save the data to local storage so that it will persist through page refreshes</p>','<a href="' + projectArray[1].url + '">' + projectArray[1].name + '</a>','<p>A project where myself and two others created a site that would allow a user to either create or find a game of tennis at their court of choice in the Seattle area</p>')
-    $('p').css({'padding-top':'2vh','margin-bottom':'1vh'})
-    $('a').css('font-size','4vh')
+$('#projects').click(function(){
+  $('#stickHere').empty()
+  projectArray.forEach(function(project){
+    $('#theProjects').append(project.toHtml())
   })
+  $('p').css({'padding-top':'1vh','margin-bottom':'1vh'})
+  $('a').css('font-size','4vh')
+})
+
+Projects.prototype.toHtml = function(){
+  var $newProject = $('#template').clone()
+  $newProject.attr('id','')
+  $newProject.find('.project a').attr('href',this.url).text(this.name)
+  $newProject.find('#description').html(this.description)
+  return $newProject
+}
+
+myProjects.forEach(function(projectData){
+  projectArray.push(new Projects(projectData))
 })
