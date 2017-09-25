2ce45c323ecb1ad88081d52f65292b4d50a9c750
Hewlette Dibble <hcodydibble@gmail.com>
2017-09-25 11:03:32 -0700

    wrapped app.js into an IIFE and added in two .maps and a .reduce

--- public/app.js
+++ public/app.js
@@ -1,61 +1,76 @@
 'use strict';
+
+var app = app || {};
 let projectArray = [], educationArray = [];
 
-function Projects(projectData){
-  Object.assign(this, projectData)
-}
+(function(module){
 
-$('.fa-bars').click(function(){
-  $('.navi').toggle(500)
-})
-
-$('#education').click(function(){
-  $('#stickHere').hide()
-  $('#theProjects').empty()
-  $('#theEducation').empty()
-  educationArray.forEach(function(education){
-    $('#theEducation').append(education.toHtml())
-  })
-})
-
-$('#projects').click(function(){
-  $('#stickHere').hide()
-  $('#theEducation').empty()
-  $('#theProjects').empty()
-  projectArray.forEach(function(project){
-    $('#theProjects').append(project.toHtml())
-  })
-  $('a').css('font-size','4vh')
-})
-
-$('#home').click(function(){
-  $('#theProjects').empty()
-  $('#theEducation').empty()
-  $('#stickHere').show()
-})
-
-Projects.prototype.toHtml = function(){
-  if(this.url){
-    let projectTemp = $('#projectTemplate').html();
-    let projectBlog = Handlebars.compile(projectTemp);
-    let newProject = new Projects(this);
-    let projectPost = projectBlog(newProject);
-    $('#theProjects').append(projectPost);
-  }else{
-    let edTemp = $('#edTemplate').html();
-    let edBlog = Handlebars.compile(edTemp);
-    let newEd = new Projects(this);
-    let edPost = edBlog(newEd);
-    $('#theEducation').append(edPost);
+  function Projects(projectData){
+    Object.assign(this, projectData)
   }
-}
 
-$.get('info.json',function(stuff){
-  stuff.forEach(function(info){
-    if(info.url){
-      projectArray.push(new Projects(info))
-    }else{
-      educationArray.push(new Projects(info))
-    }
+  $('.fa-bars').click(function(){
+    $('.navi').toggle(500)
   })
-})
+
+  $('#education').click(function(){
+    $('#stickHere').hide()
+    $('#theProjects').empty()
+    $('#theEducation').empty()
+    educationArray.forEach(function(education){
+      $('#theEducation').append(education.toHtml())
+    })
+  })
+
+  $('#projects').click(function(){
+    $('#stickHere').hide()
+    $('#theEducation').empty()
+    $('#theProjects').empty()
+    projectArray.forEach(function(project){
+      $('#theProjects').append(project.toHtml())
+    })
+    $('a').css('font-size','4vh')
+  })
+
+  $('#home').click(function(){
+    $('#theProjects').empty()
+    $('#theEducation').empty()
+    $('#stickHere').show()
+  })
+
+  Projects.prototype.toHtml = function(){
+    if(this.url){
+      let projectTemp = $('#projectTemplate').html();
+      let projectBlog = Handlebars.compile(projectTemp);
+      let newProject = new Projects(this);
+      let projectPost = projectBlog(newProject);
+      $('#theProjects').append(projectPost);
+    }else{
+      let edTemp = $('#edTemplate').html();
+      let edBlog = Handlebars.compile(edTemp);
+      let newEd = new Projects(this);
+      let edPost = edBlog(newEd);
+      $('#theEducation').append(edPost);
+    }
+  }
+
+
+  $.get('info.json',function(stuff){
+    stuff.map(function(info){
+      if(info.url){
+        projectArray.push(new Projects(info))
+      }else{
+        educationArray.push(new Projects(info))
+      }
+    })
+    Projects.numWords = () => {
+      return projectArray.map(function(a){
+        return a.description.split(' ').length
+      }).reduce(function(a,b){
+        return a + b
+      })
+    }
+    $('.totals').text(Projects.numWords())
+  })
+  module.Projects = Projects;
+})(app);
