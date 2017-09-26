79f5d7d1886fed859f712ba1c9d403d9bfed166a
Hewlette Dibble <hcodydibble@gmail.com>
2017-09-26 11:46:58 -0700

    used page.js to make my SPA a SPA A with different URLS

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -13,28 +13,28 @@ var app = app || {};
     $('.navi').toggle(500)
   })
 
-  $('#education').click(() => {
+  Projects.myEducation = function(){
     $('#stickHere').hide()
     $('#theProjects').empty()
     $('#theEducation').empty()
     educationArray.forEach(education =>
       $('#theEducation').append(education.toHtml()))
-  })
+  }
 
-  $('#projects').click(() => {
+  Projects.myProjects = function(){
     $('#stickHere').hide()
     $('#theEducation').empty()
     $('#theProjects').empty()
     projectArray.forEach(project =>
       $('#theProjects').append(project.toHtml()))
     $('a').css('font-size','4vh')
-  })
+  }
 
-  $('#home').click(() => {
+  Projects.goHome = function(){
     $('#theProjects').empty()
     $('#theEducation').empty()
     $('#stickHere').show()
-  })
+  }
 
   Projects.prototype.toHtml = function(){
     if(this.url){
